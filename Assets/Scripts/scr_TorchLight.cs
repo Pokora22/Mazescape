a1994ce9dd8c7ade6285a18c8641b57a655d9eb0
Pokora22 <pokora222@gmail.com>
2019-04-23 02:26:17 +0100

    Materials + Torches
    Some materials placed. Walls and floors loook ok, can be reused many times.
    Torch prefab done - light lerping (mixed). Need readjusting - closer to portal gates - light from other side does not help on render textures.

---
+++ Assets/Scripts/scr_TorchLight.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class scr_TorchLight : MonoBehaviour
+{
+    public float maxReduction;
+    public float maxIncrease;
+    public float flickerDelay;
+    public float strength;
+ 
+    private Light lightSource;
+    private float baseIntensity;
+    private bool _flickering;
+ 
+    public void Start()
+    {
+        lightSource = GetComponent<Light>();
+        baseIntensity = lightSource.intensity;
+        
+        StartCoroutine(doFlicker());
+    }
+ 
+    private IEnumerator doFlicker()
+    {
+        while (true)
+        {
+            lightSource.intensity = Mathf.Lerp(lightSource.intensity,
+                Random.Range(baseIntensity - maxReduction, baseIntensity + maxIncrease), strength * Time.deltaTime);
+            
+            yield return new WaitForSeconds(flickerDelay);
+        }
+    }
+    
+    private IEnumerator smoothTransition()
+    {
+        while (true)
+        {
+            lightSource.intensity = Mathf.Lerp(lightSource.intensity - 1, 
+                lightSource.intensity + 100,
+                strength * Time.deltaTime);
+            
+            yield return new WaitForSeconds(flickerDelay);
+        }
+    }
+}
