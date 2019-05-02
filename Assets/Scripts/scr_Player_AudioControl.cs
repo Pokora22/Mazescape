c60c00979bd1ae42bc3eca65c89b067bb587ecec
Pokora22 <pokora222@gmail.com>
2019-05-02 15:25:58 +0100

    Added Audio Controls for player and portals

---
+++ Assets/Scripts/scr_Player_AudioControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class scr_Player_AudioControl : MonoBehaviour
+{
+    [SerializeField] private AudioSource AmbientAudioSource;
+    [SerializeField] private AudioSource ChaseAudioSource;
+    [SerializeField] private float musicTransitionTime = 1f;
+
+    public AudioMixerSnapshot ambienceSnapshot;
+    public AudioMixerSnapshot chaseSnapshot;
+
+    
+    
+    public bool chaseMusicOn;
+
+    public void switchToChase(bool chasing)
+    {
+        chaseMusicOn = chasing;
+
+        if (chasing)
+            chaseSnapshot.TransitionTo(musicTransitionTime);
+        else
+            ambienceSnapshot.TransitionTo(musicTransitionTime);
+    }
+    
+}
---
+++ Assets/Scripts/scr_Portal_AudioControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Portal_AudioControl : MonoBehaviour
+{
+    [SerializeField] private AudioClip portalUse;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void idleSFX(bool play)
+    {
+        if(play)
+            audioSource.Play();
+        else
+            audioSource.Stop();
+    }
+
+    public void teleportSFX()
+    {
+        audioSource.PlayOneShot(portalUse);
+    }
+}
